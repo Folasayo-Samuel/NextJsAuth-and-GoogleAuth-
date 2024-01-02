f6a197a2f5f48f14e326d9c84b0416881fb353db
Saint Vandora <folasayooluwadamilare@gmail.com>
2024-01-02 10:42:03 +0100

    Update

---
+++ src/components/userAccountNav.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react'
+import { Button } from './ui/button';
+import { signOut } from 'next-auth/react';
+
+const UserAccountNav = () => {
+  return (
+    <div>
+      <Button onClick={()=> signOut({
+        redirect: true,
+        callbackUrl: `${window.location.origin}/sign-in`,
+      })} variant="destructive">Sign Out</Button>
+    </div>
+  )
+}
+
+export default UserAccountNav
---
+++ src/lib/auth.ts
@@ -0,0 +1,82 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";;
+import {PrismaAdapter} from '@next-auth/prisma-adapter';
+import {prisma} from './prisma';
+import { compare } from "bcrypt";
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions: NextAuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    secret: process.env.NEXTAUTH_SECRET,
+    session: {
+      strategy: 'jwt'
+    },
+    pages:{
+        signIn: '/sign-in',
+    },
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+      }),
+      CredentialsProvider({
+        
+        name: 'Credentials',
+        
+        credentials: {
+          email: { label: "Email", type: "text", placeholder: "johndoe@gmail.com" },
+          password: { label: "Password", type: "password" }
+        },
+        async authorize(credentials) {
+          if(!credentials?.email || !credentials?.password){
+            return null;
+          }
+
+          const existingUser = await prisma.user.findUnique({
+            where: {email: credentials?.email}
+          });
+          if(!existingUser){
+            return null;
+          }
+
+          if(existingUser.password){
+
+            const passwordMatch = await compare(credentials.password, existingUser.password);
+            
+            if(!passwordMatch){
+              return null;
+            }
+          }
+
+
+          return{
+            id: `${existingUser.id}`,
+            username: existingUser.username,
+            email: existingUser.email
+          }
+        }
+      })
+    ],
+    callbacks: {
+      async jwt({ token, user }) {
+        console.log(token, user);
+        if(user){
+          return {
+            ...token,
+            username: user.username
+          }
+        }
+        return token
+      },
+      async session({ session, user, token }) {
+        return{
+          ...session,
+          user:{
+            ...session.user,
+            username: token.username
+          }
+        }
+      },
+  }
+
+}
